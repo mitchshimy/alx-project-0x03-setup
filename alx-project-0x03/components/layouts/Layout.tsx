de3350bae8904b553f3992e174da42ea9862f5ee
shimy <ochiengmishy2016@gmail.com>
2025-06-03 13:06:13 +0300

    First commit

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import { type LayoutProps } from "@/interfaces";
+
+
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
---
+++ alx-project-0x03/interfaces/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
